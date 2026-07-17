f039259478b0b86fc2244a620da5cc4823bbf549
Hiep Nguyen <73454492+hiepdng@users.noreply.github.com>
2026-07-16 20:58:38 -0700

    Add certificate creation steps in script.sh
    Added steps to create CA, server, and client certificates using OpenSSL.

--- script.sh
+++ script.sh
@@ -30,6 +30,33 @@
 #-----------------------------------------------------------------
 
 
-# Create mount directory on the host system
+### Create mount directory on the host system
 mkdir -p /home/app/mysql
 sudo chown -R 65532:65532 /home/app/mysql
+
+
+### Create the Certificate Authority (CA): 
+# Generate CA Private Key
+openssl genrsa 2048 > ca-key.pem
+
+# Generate CA Certificate
+openssl req -new -x509 -nodes -days 3650 -key ca-key.pem -out ca.pem -subj '/C=US/ST=state/L=city/O=example/OU=Sale-Dept/CN=server.example.com/emailAddress=your@example.com'
+
+
+### Create the Server Certificate:
+# Generate Server Private Key
+openssl req -newkey rsa:2048 -nodes -days 3650 -keyout server-key.pem -out server-req.pem -subj '/C=US/ST=state/L=city/O=example/OU=Sale-Dept/CN=server.example.com/emailAddress=your@example.com'
+
+# Sign the Server Certificate with the CA
+openssl x509 -req -days 3650 -in server-req.pem -CA ca.pem -CAkey ca-key.pem -set_serial 01 -out server-cert.pem
+
+
+### Create the Client Certificate:
+# Generate Client Private Key
+openssl req -newkey rsa:2048 -days 3650 -nodes -keyout client-key.pem -out client-req.pem -subj '/C=US/ST=state/L=city/O=example/OU=Sale-Dept/CN=client.example.com/emailAddress=your@example.com'
+
+# Sign the Client Certificate with the CA
+openssl x509 -req -days 3650 -in client-req.pem -CA ca.pem -CAkey ca-key.pem -set_serial 01 -out client-cert.pem
+
+
+
